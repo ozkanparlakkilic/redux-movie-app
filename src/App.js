a46e99f19a97cbc25789ae187b660db686a28672
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-05-03 04:14:11 +0300

    style app component

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ function App() {
     <div className="app">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/movie/:imdbID" component={MovieDetails} />
-          <Route component={PageNotFound} />
-        </Switch>
-
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/movie/:imdbID" component={MovieDetails} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
         <Footer />
       </Router>
     </div>
