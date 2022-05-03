34c63c76fd395fa8d4f855ba34b80843b47c1dec
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-05-04 02:17:17 +0300

    add home component with data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err);
+        });
+      dispatch(addMovies(response.data));
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
