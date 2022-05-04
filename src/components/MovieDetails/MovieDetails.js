6c387ce56dd37c26ef1fd09636e953d8b1aa49ad
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-05-04 03:56:00 +0300

    add loading

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -21,49 +21,56 @@ const MovieDetails = () => {
 
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <i className="fa fa-film"></i> : {data.Runtime}
-          </span>
-          <span>
-            Year <i className="fa fa-calendar"></i> : {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).lenght ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i> :{" "}
+                {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"></i> : {data.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i> : {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Actors</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Generes</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Actors</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
