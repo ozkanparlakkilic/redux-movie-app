3663d7f5aaeffda78f26715a68fb24ff2a5a915f
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-05-04 04:28:47 +0300

    add search bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,9 +8,11 @@ import {
 
 const Home = () => {
   const dispatch = useDispatch();
+  const movieText = "Harry";
+  const showText = "Friends";
   useEffect(() => {
-    dispatch(fetchAsyncMovies());
-    dispatch(fetchAsyncShows());
+    dispatch(fetchAsyncMovies(movieText));
+    dispatch(fetchAsyncShows(showText));
   }, [dispatch]);
 
   return (
