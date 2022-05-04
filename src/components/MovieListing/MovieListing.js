290e7f4e1833dcf5471b92647aeb6a28c932c28b
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-05-04 03:01:47 +0300

    get shows data and add style

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import { useSelector } from "react-redux";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
 
   let renderMovies = "";
+  let renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
@@ -20,11 +22,26 @@ const MovieListing = () => {
       </div>
     );
 
+  renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) => {
+        return <MovieCard key={index} data={show} />;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
